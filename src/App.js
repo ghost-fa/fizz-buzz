39cf6270e78dba2682d4dacc5ad8a0ea7053572e
ghost <fayyad_r22@yahoo.com>
2019-01-30 21:32:40 +0100

    add state to the component and add function to generate array of numbers

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    fizzBuzzBox: null,
+    fizzBuzzCounter: 0,
+    limitNumber: 100
+  };
+
+  NumbersGenerator = limit => {
+    if (limit) {
+      const Box = [];
+      for (let i = 1; i <= limit; i++) {
+        Box.push(i);
+      }
+      if (Box.length === limit) {
+        this.setState({
+          numbers: Box
+        });
+      }
+      return Box;
+    }
+  };
   render() {
     return <div className="App" />;
   }
